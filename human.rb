68419d1de1f3397866ca9a5140c6305962070029
Hiro-o-ai <110434009@ccalumni.meijo-u.ac.jp>
2020-08-31 01:05:23 +0900

    create_human_and_manager

---
+++ human.rb
@@ -0,0 +1,14 @@
+class Human
+  def initialize(name, height)
+    @name = name
+    @height = height
+  end
+
+  def eat
+    puts '食べています'
+  end
+
+  def sleep
+    puts '寝てます'
+  end
+end
---
+++ manager.rb
@@ -0,0 +1,5 @@
+require_relative './human'
+
+human = Human.new("小林", 178)
+human.eat
+human.sleep
